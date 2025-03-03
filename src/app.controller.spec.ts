a633227e02c73a95302a68eb0a52263c05995f5c
shardulsingh <shardul@particle41.com>
2025-03-03 14:44:49 -0500

    BVP-60: fixed bad commit

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Not Hello World!');
+      expect(appController.getHello()).toBe('Hello World!');
     });
   });
 });
