69bc833f795e19c110b4142e3948d60b6893a08b
shardulsingh <shardul@particle41.com>
2025-03-11 01:09:33 -0400

    BVP-15: update configuration for eslint

--- src/config/application.configuration.ts
+++ src/config/application.configuration.ts
@@ -1,7 +1,9 @@
 interface IApplicationConfiguration {
   port: number;
+  jwtSecret: string;
 }
 
 export default (): IApplicationConfiguration => ({
   port: +process.env.PORT!,
+  jwtSecret: process.env.JWT_SECRET!,
 });
