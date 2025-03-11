a638d7da81edb46c1d0386ba2cdba6c0feb404c2
shardulsingh <shardul@particle41.com>
2025-03-11 00:34:46 -0400

    BVP-15: updated constants and dto classes

--- src/excpetions/credentials.exception.ts
+++ src/excpetions/credentials.exception.ts
@@ -1,8 +1,17 @@
 import { HttpException, HttpStatus } from '@nestjs/common';
-import { InvalidCredentialsErrorResponse } from '../utils/constants/api-response.constants';
+import {
+  InvalidCredentialsErrorResponse,
+  UserNotFoundErrorResponse,
+} from '../utils/constants/api-response.constants';
 
 export class InvalidUserCredentialsException extends HttpException {
   constructor() {
     super(InvalidCredentialsErrorResponse, HttpStatus.UNAUTHORIZED);
+  }
+}
+
+export class UserNotFoundException extends HttpException {
+  constructor() {
+    super(UserNotFoundErrorResponse, HttpStatus.NOT_FOUND);
   }
 }
