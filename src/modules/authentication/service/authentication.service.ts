037e1ea0716602bc0cf86f8bf0b6bd92c8b3d26b
shardulsingh <shardul@particle41.com>
2025-03-13 15:08:06 -0400

    BVP-15: refactor `signIn` -> `validateUser`

--- src/modules/authentication/service/authentication.service.ts
+++ src/modules/authentication/service/authentication.service.ts
@@ -2,10 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { UserService } from '../../user/service/user.service';
 
 import * as bcrypt from 'bcrypt';
-import {
-  InvalidUserCredentialsException,
-  UserNotFoundException,
-} from '../../../excpetions/credentials.exception';
+import { InvalidUserCredentialsException } from '../../../excpetions/credentials.exception';
 import { JwtService } from '@nestjs/jwt';
 import { SuccessfulLoginResponse } from '../../../interfaces/api/response/api.response';
 import { LoginRequestDto } from '../dto/login-request.dto';
@@ -21,40 +18,28 @@ export class AuthenticationService {
     private readonly jwtService: JwtService,
   ) {}
 
-  async signIn(
+  async validateUser(
     loginRequestDto: LoginRequestDto,
   ): Promise<SuccessfulLoginResponse> {
-    try {
-      const user = await this.userService.findOneByEmail(loginRequestDto.email);
-      const isPasswordMatched = await this.compareHash(
-        loginRequestDto.password,
-        user.password,
-      );
-      if (!isPasswordMatched) {
-        throw new InvalidUserCredentialsException();
-      }
-      const sanitizedUserData = {
-        id: user.id,
-        first_name: user.first_name,
-        last_name: user.last_name,
-        email: user.email,
-        created_at: user.created_at,
-      };
-      return {
-        access_token: await this.jwtService.signAsync({
-          id: user.id,
-          first_name: user.first_name,
-          last_name: user.last_name,
-          email: user.email,
-        }),
-        ...sanitizedUserData,
-      };
-    } catch (error: unknown) {
-      if (error instanceof UserNotFoundException) {
-        throw new InvalidUserCredentialsException();
-      }
-      throw error;
+    const user = await this.userService.findOneByEmail(loginRequestDto.email);
+    const isPasswordMatched = await this.compareHash(
+      loginRequestDto.password,
+      user.password,
+    );
+    if (!isPasswordMatched) {
+      throw new InvalidUserCredentialsException();
     }
+    const sanitizedUserData = {
+      id: user.id,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      email: user.email,
+      created_at: user.created_at,
+    };
+    return {
+      access_token: await this.jwtService.signAsync(sanitizedUserData),
+      ...sanitizedUserData,
+    };
   }
 
   async register(createUserDto: CreateUserDto): Promise<User> {
