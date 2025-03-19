9ab3cd6ad8a1e9aa0ad6759f6afe32c850846f48
shardulsingh <shardul@particle41.com>
2025-03-18 23:31:33 -0400

    BVP-21: role constants & entity schema

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -31,7 +31,6 @@ export default tseslint.config(
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
       '@typescript-eslint/unbound-method': 'error',
-      'no-eslint-disable-next-line': 'error',
     },
   },
 );
