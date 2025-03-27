0de7ce123cd3be5b417bc9cb2a502cf42c65ada8
shardulsingh <shardul@particle41.com>
2025-03-26 23:36:48 -0400

    BVP-26: removed unnecessary dto

--- src/modules/venue/dto/find-venues-by-organization.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsNumber, IsPositive } from 'class-validator';
-
-export class FindVenuesByOrganizationDto {
-  @IsNumber({ maxDecimalPlaces: 0 })
-  @IsPositive()
-  organization: number;
-}
