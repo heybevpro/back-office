aa16709eb6c510760844eb2e8e01e78e2ac13e5e
shardulsingh <shardul@particle41.com>
2025-04-01 23:56:12 -0700

    BVP-26: updated dto

--- src/modules/venue/dto/create-venue.dto.ts
+++ src/modules/venue/dto/create-venue.dto.ts
@@ -1,8 +1,36 @@
-import { IsNumber, IsPositive, IsString } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsPhoneNumber,
+  IsPositive,
+  IsString,
+  MaxLength,
+} from 'class-validator';
 
 export class CreateVenueDto {
   @IsString()
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  address: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(32)
+  city: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(20)
+  state: string;
+
+  @IsPhoneNumber()
+  phone_number: string;
+
+  @IsNumber()
+  @IsPositive()
+  capacity: number;
 
   @IsNumber({ maxDecimalPlaces: 0, allowInfinity: false })
   @IsPositive()
