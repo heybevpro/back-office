68d68c177bc7e1f1979d859bcf5f8bb96b23bb76
shardulsingh <shardul@particle41.com>
2025-04-08 01:22:23 -0400

    BVP-36: update circular dependencies

--- src/modules/role/role.module.ts
+++ src/modules/role/role.module.ts
@@ -1,12 +1,14 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Role } from './entity/role.entity';
 import { RoleService } from './service/role.service';
+import { RoleController } from './controller/role.controller';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Role])],
+  imports: [TypeOrmModule.forFeature([Role]), forwardRef(() => UserModule)],
   providers: [RoleService],
-  // controllers: [],
+  controllers: [RoleController],
   exports: [RoleService],
 })
 export class RoleModule {}
