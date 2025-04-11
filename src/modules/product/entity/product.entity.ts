b5e2c0e093b4eb3f66a31a93e055b70c825146cd
shardulsingh <shardul@particle41.com>
2025-04-11 01:00:31 -0400

    BVP-141: product entity & migration

---
+++ src/modules/product/entity/product.entity.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { ProductType } from '../../product-type/entity/product-type.entity';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', length: 128 })
+  name: string;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  price: number;
+
+  @Column('text', { nullable: true, default: null })
+  description: string;
+
+  @ManyToOne(() => ProductType, (productType) => productType.products, {
+    nullable: false,
+  })
+  product_type: ProductType;
+}
