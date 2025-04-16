c734c75a7349cf09ab8125a946af413987b7abf7
Dheeraj Thakur <dheeraj.thakur@particle41.com>
2025-04-16 12:08:47 +0530

    BVP-141: fix product name validation

--- src/modules/product/dto/create-product.dto.ts
+++ src/modules/product/dto/create-product.dto.ts
@@ -4,11 +4,13 @@ import {
   IsOptional,
   IsString,
   IsUUID,
+  MaxLength,
 } from 'class-validator';
 
 export class CreateProductDto {
   @IsString()
   @IsNotEmpty()
+  @MaxLength(128)
   name: string;
 
   @IsDecimal()
