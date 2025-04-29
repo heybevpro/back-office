d5a8433b888a291e995303accf11816d4868b0e5
shardulsingh <shardul@particle41.com>
2025-04-29 11:00:38 -0400

    BVP-69: password request validation guards

---
+++ src/modules/authentication/strategy/temporary-access/temporary-access.strategy.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { EnvironmentVariable } from '../../../../utils/constants/environmentType';
+import { AuthenticationService } from '../../service/authentication.service';
+import { VerifiedJwtPayload } from '../../../../utils/constants/auth.constants';
+
+@Injectable()
+export class TemporaryAccessStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly authService: AuthenticationService,
+  ) {
+    super({
+      ignoreExpiration: false,
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: configService.get<EnvironmentVariable.JWT_SECRET>(
+        EnvironmentVariable.JWT_SECRET,
+      )!,
+    });
+  }
+
+  validate = (payload: VerifiedJwtPayload) => {
+    console.log('TemporaryAccessStrategy validate payload:', payload);
+    return payload;
+  };
+}
