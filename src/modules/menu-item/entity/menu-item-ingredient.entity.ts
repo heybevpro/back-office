30a47d9e306e7ae4a3a269259d48b322d1fc7f8c
shardulsingh <shardul@particle41.com>
2025-07-10 17:29:59 -0400

    BVP-295: menu-item and ingredient migration and entity

---
+++ src/modules/menu-item/entity/menu-item-ingredient.entity.ts
@@ -0,0 +1,38 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Product } from '../../product/entity/product.entity';
+import { ServingSize } from '../../serving-size/entity/serving-size.entity';
+import { MenuItem } from './menu-item.entity';
+
+@Entity()
+export class MenuItemIngredient {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @ManyToOne(() => Product, { nullable: false })
+  product: Product;
+
+  @Column({ type: 'int', unsigned: true, nullable: false })
+  quantity: number;
+
+  @ManyToOne(() => ServingSize, { nullable: true })
+  serving_size: ServingSize;
+
+  @ManyToOne(() => MenuItem, (menuItem) => menuItem.ingredients, {
+    nullable: false,
+    onDelete: 'RESTRICT',
+  })
+  menu_item: MenuItem;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
